2703e741ccd51052b39d5b390997090f9bf19fb7
NatsNatsik <nastena.erokhina.94@mail.ru>
2022-11-03 20:13:25 +0300

    Добавлены звуки, шейдеры, частицы и исправлены некоторые баги

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 
 public class Finish : MonoBehaviour
 {
+
+    public AudioSource FinishAudio;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.TryGetComponent(out Snake snake))
         {
             snake.ReachFinish();
+            FinishAudio.PlayDelayed(0.25f);
 
         }
 }
